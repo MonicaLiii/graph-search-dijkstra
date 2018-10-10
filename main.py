e3d6c83f9fdcd8275f9c4d16633949e2401ea005
MonicaLiii <liy@union.edu>
2018-10-10 07:42:30 -0400

    Finished

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 import heapq, sys
 
+
 class PriorityQueue:
     """
       Applies the priority queue data structure.
@@ -15,15 +16,13 @@ class PriorityQueue:
     """
     def  __init__(self):
         self.heap = []
-        self.count = 0
 
     def push(self, item, priority):
-        entry = (priority, self.count, item)
+        entry = (priority, item)
         heapq.heappush(self.heap, entry)
-        self.count += 1
 
     def pop(self):
-        (priority, _, item) = heapq.heappop(self.heap)
+        priority, item = heapq.heappop(self.heap)
         return (item, priority)
 
     def isEmpty(self):
@@ -32,24 +31,19 @@ class PriorityQueue:
 
 class Graph:
 
-    class Edge:
-        def __init__(self, left, right, cost):
-            self.left = left
-            self.right = right
-            self.cost = cost
-
     def __init__(self, vertex_num, edge_num, root):
         self.vertex_num = vertex_num
-        self.edge_num = edge_num
         self.root = root
         self.vertices = []
         self.dist = []
         for i in range(0, vertex_num):
             self.vertices.append([])
+            #print(self.vertices)
             self.dist.append(100000)
+            #print(self.dist)
 
-    def addEdge(self, left, right, cost):
-        self.vertices[left].append(self.Edge(left, right, cost))
+    def addEdge(self, s, t, d):
+        self.vertices[s].append((t, d))
 
     def dijkstra(self):
 
@@ -57,35 +51,40 @@ class Graph:
         q.push(self.root, 0)
         self.dist[self.root] = 0
 
-        while q:
-            u_cost, u = q.pop()
-            if self.dist[u] < u_cost:
-                continue
-            for e in self.vertices[u]:
-                if self.dist[e.right] > u_cost + e.cost:
-                    self.dist[e.right] = u_cost + e.cost
-                    q.push(e.right, self.dist[e.right])
+        while not q.isEmpty():
+            u, u_cost = q.pop()
+            #print(u)
+            #print(u_cost)
+            for edge in self.vertices[u]:
+                t = edge[0]
+                #print(t)
+                d = edge[1]
+                if self.dist[t] > u_cost + d:
+                    self.dist[t] = u_cost + d
+                    q.push(t, self.dist[t])
 
 
 if __name__ == "__main__":
     print("Enter your inputs here:")
-    input = sys.stdin.readline()
-    V = int(input[0])
-    E = int(input[2])
-    r = int(input[4])
+    input_graph = sys.stdin.readline()
+    input_graph = input_graph.split(" ")
+    V = int(input_graph[0])
+    E = int(input_graph[1])
+    r = int(input_graph[2])
     problem = Graph(V, E, r)
 
     for i in range(0, E):
-        input2 = sys.stdin.readline()
-        left = int(input2[0])
-        right = int(input2[2])
-        cost = int(input2[4])
-        problem.addEdge(left, right, cost)
+        input_edges = sys.stdin.readline()
+        input_edges = input_edges.split(" ")
+        s = int(input_edges[0])
+        t = int(input_edges[1])
+        d = int(input_edges[2])
+        problem.addEdge(s, t, d)
 
     problem.dijkstra()
 
     for v in problem.dist:
-        if v == 999:
+        if v == 100000:
             print("INF")
         else:
             print(v)
